3d7bcf839117f7629e3526cb9e0e80bcd1399914
Juan C Galvez <jcgalvezv@gmail.com>
2023-05-17 15:25:49 -0600

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
-create table telefonos (id binary(16) not null, city_code varchar(2) not null, country_code varchar(3) not null, number varchar(12) not null, primary key (id));
+create table telefonos (id binary(16) not null, citycode varchar(2) not null, countrycode varchar(3) not null, number varchar(12) not null, primary key (id));
 create table usuarios (id binary(16) not null, created timestamp, email varchar(60) not null, isactive boolean, jwt varchar(1024), last_login timestamp, modified timestamp, name varchar(60) not null, password varchar(30) not null, primary key (id));
 create table usuarios_telefonos (usuario_id binary(16) not null, telefono_id binary(16) not null);
 alter table usuarios add constraint usuario_email_idx unique (email);
