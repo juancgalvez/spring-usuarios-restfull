a125df9d2ea4189b8ccd3267fa8385d9851e71e3
Juan C Galvez <jcgalvezv@gmail.com>
2023-05-17 15:37:29 -0600

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into telefonos (id, number, city_code, country_code) values (x'41a7a2af3a2849a99311efd3273cc5d7','12345679','2','56');
-insert into telefonos (id, number, city_code, country_code) values (x'a5525293fe29412ab7f3bcac0cbe4a10','87654321','55','56');
+insert into telefonos (id, number, citycode, countrycode) values (x'41a7a2af3a2849a99311efd3273cc5d7','12345679','2','56');
+insert into telefonos (id, number, citycode, countrycode) values (x'a5525293fe29412ab7f3bcac0cbe4a10','87654321','55','56');
 
 insert into usuarios (id, created, email, isactive, jwt, last_login, modified, name, password) values (x'd0761d73cbee4a56b8bc9e5aadefea66','2023-04-28 13:32:09.391','jcgalvezv@localhost.cl',TRUE,null,'2023-04-28 13:32:09.391','2023-04-28 13:32:09.391','Juan Carlos Galvez Villegas','P@ssw0rd');
 
